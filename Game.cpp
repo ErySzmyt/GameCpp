7431a4dc317799fa07bd0591ad62737674373192
ErySzmyt <erykszm222@student.polsl.pl>
2021-03-12 19:57:12 +0100

    Beta 1

--- Game.cpp
+++ Game.cpp
@@ -1,10 +1,13 @@
-﻿#define _USE_MATH_DEFINES
-
-#include<allegro5/allegro.h>													
+﻿#include<allegro5/allegro.h>													
 #include<allegro5/allegro_font.h>	
 #include <allegro5/allegro_primitives.h>
-#include<cstdio>
-#include<iostream>
+#include <Windows.h>
+#include <iostream>
+#include <cstdlib>
+
+#include <chrono>
+#include <thread>
+
 #include<math.h>
 #include "Game.h";
 
@@ -29,7 +32,7 @@ double losujKierunek()
 {
 	// funkcja losuje  wartosci kierunku double 0-360
 	const int min = 0;
-	const int max = 360;
+	const int max = 10;
 	double los = (double)rand() / RAND_MAX;
 	return min + los * (max - min);
 }
@@ -49,7 +52,6 @@ double przeliczCosNaKat()
 }
 
 //double x1, double x2, double y1, double y2
-
 double wyliczCosinus(Vector v1,Vector v2)
 {
 	double upper = ((v1.dir_x * v2.dir_x) + (v1.dir_y * v2.dir_y));
@@ -58,45 +60,36 @@ double wyliczCosinus(Vector v1,Vector v2)
 	return cos(upper / lower);
 }
 
-
-double reverseDirX(Vector v1)
+void reverseDirX(Vector& v1)
 {
-	const float wart = 1 / 10;
-	double wyn = v1.dir_x * wart;
-	return -(v1.dir_x + wyn);
+	double wyn = v1.dir_x + (rand() % 3);
+	v1.dir_x = -(v1.dir_x + wyn);
 }
 
-double reverseDirY(Vector v1)
+void reverseDirY(Vector& v1)
 {
 	// zwraca zanegowy wektor
-	const float wart = 1 / 10;
-	double wyn = v1.dir_y * wart;
-	return -(v1.dir_y + wyn);
+	double wyn = v1.dir_y + (rand() % 3);
+	v1.dir_y = -(v1.dir_y + wyn);
 }
 
 Vector randVector() {
 	Vector temp = Vector();
-	temp.dir_x = losujKierunek();
-	temp.dir_y = losujKierunek();
+	temp.dir_x = 4;
+	temp.dir_y = 4;
 
 	return temp;
 }
 
+void drawCircle(Circle circle) {
+	al_draw_filled_rectangle(circle.pos_x, circle.pos_y, circle.pos_x + 30, circle.pos_y + 30, al_map_rgb(0, 0, 255));
+}
+
 // </funkcje>
-void main(void) {
-	Vector top = Vector();
-	top.dir_x = 4;
-	top.dir_y = 3;
+int main() {
+	/* initialize random seed: */
+	srand(time(NULL));
 
-
-	Vector customV = Vector();
-	customV.dir_x = 2;
-	customV.dir_y = 5;
-
-	std::cout << wyliczCosinus(top, customV);
-
-
-	/*
 	al_init();
 	al_init_primitives_addon();
 	al_install_keyboard();
@@ -109,32 +102,44 @@ void main(void) {
 	al_register_event_source(event_queue, al_get_keyboard_event_source());
 
 
-	int pos_x = DISP_WIDTH / 2;
-	int pos_y = DISP_HEIGHT / 2;	
-
-
-
 	bool running = true;
+
+	Circle circle = Circle();
+	circle.pos_x = DISP_WIDTH / 2;
+	circle.pos_y = DISP_HEIGHT / 2;
+	circle.vector = randVector();
 
 	// Main loop
 	while (running) {
-		ALLEGRO_EVENT ev;
+		//ALLEGRO_EVENT ev;
 		//std::cout << "ddd";
 		//al_wait_for_event(event_queue, &ev);
 
+		circle.pos_x += circle.vector.dir_x;
+		circle.pos_y += circle.vector.dir_y;
+
+		if (detectCollisionVertically(circle.pos_x, circle.pos_y, circle.pos_x+30, circle.pos_y+30)) {
+			reverseDirY(circle.vector);
+		}
+
+
+		if (detectCollisionHorisontally(circle.pos_x, circle.pos_y, circle.pos_x + 30, circle.pos_y + 30)) {
+			reverseDirX(circle.vector);
+		}
 		
 
 
-		al_draw_filled_rectangle(pos_x, pos_y, pos_x + 30, pos_y + 30, al_map_rgb(0,0,255));
+		//drawCircle(circle);
+		drawCircle(circle);
 
 		al_flip_display();
 
 		al_clear_to_color(al_map_rgb(0, 0, 0));
 
-		_sleep(40);
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
 	}
 	al_destroy_display(disp);
 
-	*/
+	
 }
